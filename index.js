487f9d2a45ea3184297de7d7a2a2f9cb79ebd94d
Jesse Yang <jyyjcc@gmail.com>
2014-04-24 22:38:42 +0800

    make it work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Cacheable = require('cacheable')
 var co = require('co')
 var util = require('util')
 var __slice = [].slice
+var _REALNAME = Cacheable._REALNAME
 
 
 function CoCacheable() {
@@ -12,8 +13,10 @@ util.inherits(CoCacheable, Cacheable)
 
 CoCacheable.prototype.wrap = function(fn) {
   var args = __slice.call(arguments)
+  var isGen = isGeneratorFunction(fn)
+  var wrapped
 
-  if (isGeneratorFunction(fn)) {
+  if (isGen) {
     // convert generator function to async mode
     args[0] = function() {
       var params = __slice.call(arguments)
@@ -28,8 +31,26 @@ CoCacheable.prototype.wrap = function(fn) {
         next(err, result)
       }).call(this)
     }
+    // method must provide a name
+    args[0][_REALNAME] = fn[_REALNAME]
   }
-  Cacheable.prototype.wrap.apply(this, args)
+
+  wrapped = Cacheable.prototype.wrap.apply(this, args)
+
+  if (isGen) {
+    var _wrapped = wrapped
+    // convert the wrapped async function to thunk,
+    // so we can `yield` it
+    wrapped = function() {
+      var params = __slice.call(arguments)
+      var self = this
+      return function(next) {
+        params.push(next)
+        _wrapped.apply(self, params)
+      }
+    }
+  }
+  return wrapped
 }
 
 
